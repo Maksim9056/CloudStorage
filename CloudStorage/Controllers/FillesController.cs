dd26c4eb991790330c814d7522473068c2e52709
Maks <bobreczovm@inbox.ru>
2024-05-07 21:41:42 +0300

    Правки работает

--- CloudStorage/Controllers/FillesController.cs
+++ CloudStorage/Controllers/FillesController.cs
@@ -15,20 +15,66 @@ namespace CloudStorageWebAPI.Controllers
     public class FillesController : ControllerBase
     {
         private readonly DB _context;
-        string path = AppDomain.CurrentDomain.BaseDirectory;
 
         public FillesController(DB context)
         {
             _context = context;
         }
+        [HttpPost]
+        public async Task<ActionResult<Filles>> PostFilles(Filles filles)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory;
+                string fileExtension = Path.GetExtension(filles.NameFille).Trim('.');
+                filles.TypeFiles = fileExtension;
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == filles.UserId);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                if (!Directory.Exists(path + $"\\{user.Name}"))
+                {
+                    Directory.CreateDirectory(path + $"\\{user.Name}");
+                    using (MemoryStream memoryStream = new MemoryStream(filles.Fille))
+                    {
+                        using (FileStream fileStream = new FileStream(path + $"\\{user.Name}\\{filles.NameFille}", FileMode.OpenOrCreate))
+                        {
+                            await memoryStream.CopyToAsync(fileStream);
+                            filles.StoragePath = fileStream.Name;
+                            filles.Size = fileStream.Length;
+                        }
+                    }
 
-        //// GET: api/Filles
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Filles>>> GetGroupsChats()
-        //{
-        //    return await _context.GroupsChats.Where.ToListAsync();
-        //}
 
+                    Console.WriteLine($"Папка успешно создана! {path + $"\\{user.Name}"}");
+                }
+                else
+                {
+                    using (MemoryStream memoryStream = new MemoryStream(filles.Fille))
+                    {
+                        using (FileStream fileStream = new FileStream(path + $"\\{user.Name}\\{filles.NameFille}", FileMode.OpenOrCreate))
+                        {
+                            await memoryStream.CopyToAsync(fileStream);
+                            filles.StoragePath = fileStream.Name;
+
+                            filles.Size = fileStream.Length;
+                        }
+                    }
+                    Console.WriteLine($"Папка с указанным путем уже существует: {path + $"\\{user.Name}"}");
+                }
+
+                _context.Filles.Add(filles);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetFilles", new { id = filles.Id }, filles);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при сохранении файла: {ex.Message}");
+                return StatusCode(500, "Internal server error.");
+            }
+        }
         [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<Filles>>> GetGroupsChats(int id)
         {
@@ -37,11 +83,47 @@ namespace CloudStorageWebAPI.Controllers
                 return await _context.Filles.Where(u => u.UserId == id).ToListAsync();
 
             }
-            catch(Exception ex) 
+            catch (Exception ex)
             {
                 return NotFound();
             }
         }
+
+        // DELETE: api/Filles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFilles(int id)
+        {
+            try
+            {
+
+
+                var filles = await _context.Filles.FindAsync(id);
+                if (filles == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Filles.Remove(filles);
+                await _context.SaveChangesAsync();
+                System.IO.File.Delete(filles.StoragePath);
+
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex);
+
+
+            }
+            return NoContent();
+        }
+
+        //// GET: api/Filles
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Filles>>> GetFilles()
+        //{
+        //    return await _context.Filles.ToListAsync();
+        //}
+
         //// GET: api/Filles/5
         //[HttpGet("{id}")]
         //public async Task<ActionResult<Filles>> GetFilles(int id)
@@ -87,92 +169,36 @@ namespace CloudStorageWebAPI.Controllers
         //    return NoContent();
         //}
 
-        // POST: api/Filles
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Filles>> PostFilles(Filles filles)
-        {
-            try
-            {
-                string fileExtension = Path.GetExtension(filles.NameFille).Trim('.');
-                filles.TypeFiles = fileExtension;
-                var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == filles.UserId);
-                if (user == null)
-                {
-                    return NotFound();
-                }
-                if (!Directory.Exists(path + $"\\{user.Name}"))
-                {
-                    Directory.CreateDirectory(path + $"\\{user.Name}");
-                    using (MemoryStream memoryStream = new MemoryStream(filles.Fille))
-                    {
-                        using (FileStream fileStream = new FileStream(path + $"\\{user.Name}\\{filles.NameFille}", FileMode.OpenOrCreate))
-                        {
-                             await    memoryStream.CopyToAsync(fileStream);
-                            filles.StoragePath = fileStream.Name;
-                            filles.Size = fileStream.Length;
-                        }
-                    }
+        //// POST: api/Filles
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Filles>> PostFilles(Filles filles)
+        //{
+        //    _context.Filles.Add(filles);
+        //    await _context.SaveChangesAsync();
 
+        //    return CreatedAtAction("GetFilles", new { id = filles.Id }, filles);
+        //}
 
-                    Console.WriteLine($"Папка успешно создана! {path + $"\\{user.Name}"}");
-                }
-                else
-                {
-                    using (MemoryStream memoryStream = new MemoryStream(filles.Fille))
-                    {
-                        using (FileStream fileStream = new FileStream(path + $"\\{user.Name}\\{filles.NameFille}", FileMode.OpenOrCreate))
-                        {
-                            await memoryStream.CopyToAsync(fileStream);
-                            filles.StoragePath = fileStream.Name;
+        //// DELETE: api/Filles/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteFilles(int id)
+        //{
+        //    var filles = await _context.Filles.FindAsync(id);
+        //    if (filles == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-                            filles.Size = fileStream.Length;
-                        }
-                    }
-                    Console.WriteLine($"Папка с указанным путем уже существует: {path + $"\\{user.Name}"}");
-                }
+        //    _context.Filles.Remove(filles);
+        //    await _context.SaveChangesAsync();
 
-                _context.Filles.Add(filles);
-                await _context.SaveChangesAsync();
+        //    return NoContent();
+        //}
 
-            }
-            catch (Exception ex)
-            {              
-                NotFound(ex);
-            }
-            return CreatedAtAction("GetFilles", new { id = filles.Id }, filles);
-        }
-
-        // DELETE: api/Filles/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteFilles(int id)
-        {
-            try
-            {
-
-
-                var filles = await _context.Filles.FindAsync(id);
-                if (filles == null)
-                {
-                    return NotFound();
-                }
-
-                _context.Filles.Remove(filles);
-                await _context.SaveChangesAsync();
-                System.IO.File.Delete(filles.StoragePath);
-
-            }catch (Exception ex)
-            {
-                return NotFound(ex);
-
-
-            }
-            return NoContent();
-        }
-
-        private bool FillesExists(int id)
-        {
-            return _context.Filles.Any(e => e.Id == id);
-        }
+        //private bool FillesExists(int id)
+        //{
+        //    return _context.Filles.Any(e => e.Id == id);
+        //}
     }
 }
