58690b6aca2ce8ad5ea129a94f8a7e791a257f9c
Maks <bobreczovm@inbox.ru>
2024-05-11 12:24:57 +0300

    работает кросплатформенно

--- CloudStorage/Program.cs
+++ CloudStorage/Program.cs
@@ -1,7 +1,8 @@
 
+using CloudStorageClass.CloudStorageModel;
 using CloudStorageWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
 
 namespace CloudStorage
 {
@@ -12,14 +13,24 @@ namespace CloudStorage
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine("Путь: " + path);
+
+            string paths = Path.Combine(path, "appsettings.json");
+            Console.WriteLine("Путь и файл: " + paths);
+
+            var aCS = builder.Configuration.AddJsonFile(paths).Build().GetSection("ConnectionStrings")["CloudStorageAPI"];
+            //var aCS = builder.Configuration.GetConnectionString("CloudStorageAPI");
+            Console.WriteLine("Строка подключения из настроек: "+aCS);
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<DB>(options =>
-            //options.UseNpgsql(builder.Configuration.GetConnectionString("CloudStorageAPI") ?? throw new InvalidOperationException("Connection string 'CloudStorageAPI' not found.")));
-            options.UseNpgsql("Host=localhost;Port=5432;Database=CloudStorage;Username=postgres;Password=2" ?? throw new InvalidOperationException("Connection string 'CloudStorageAPI' not found.")));
+            options.UseNpgsql(aCS ?? throw new InvalidOperationException("Connection string 'CloudStorageAPI' not found.")));
+            //options.UseNpgsql("Host=localhost;Port=5432;Database=CloudStorage;Username=postgres;Password=2" ?? throw new InvalidOperationException("Connection string 'CloudStorageAPI' not found.")));
+
 
             var app = builder.Build();
 
